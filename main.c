ebe4e7ddb71df4104e5c9e8cfaf91da5e2125a34
Fredmaster08 <frxdmaster@gmail.com>
2026-04-26 11:36:23 +0200

    FIXED LOGIC

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <locale.h>
-
-#define WORD
+#include <time.h>
 
 void init_board(const char* words[], char board[]) {
     int length = strlen(*words);
@@ -13,12 +12,24 @@ void init_board(const char* words[], char board[]) {
     }
 }
 
+const char* get_random_word(const char* words[], int count) {
+    count = sizeof(*words) / sizeof(*words[0]);
+    int random_word = rand() % count;
+    const char* word = words[random_word];
+    return word;
+}
+
 void print_board(char board[], int length) {
-       
+    for (int i = 0; i < length; i++) {
+        printf("%c", board[i]);
+        printf(" ");
+    }
+    printf("\n");
 }
 
 int main() {
     setlocale(LC_ALL, "");
+    srand(time(NULL));
 
     const char* words[] = {
         "hangman",
@@ -28,16 +39,22 @@ int main() {
         "Kakerlake"
     };
 
-    char board[] = {'_'};
-    int length = 0;
+    int count = sizeof(words) / sizeof(words[0]);
+    int random_index = rand() % count;
+    int random_word_len = strlen(words[random_index]);
+
+    char board[random_word_len + 1];
+    for (int i = 0; i < random_word_len; i++) {
+        board[i] = '_';
+    }
+
+    board[random_word_len] = '\0';
+    for (int i = 0; i < )
+
+
+    printf("%s\n%s\n", words[random_index], board);
     
 
-    init_board(words, board);
-    fflush(stdout);
-
-    while(1) {
-        
-    }
 
     return 0;
 }
