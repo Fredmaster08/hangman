82d4ca56959af90392e1b9596a1ecf3ed19926bd
Fredmaster08 <frxdmaster@gmail.com>
2026-04-26 15:49:59 +0200

    removed functions, only main()

--- main.c
+++ main.c
@@ -1,31 +1,10 @@
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <time.h>
-
-void init_board(const char* words[], char board[]) {
-    int length = strlen(*words);
-    for (int i = 0; i < length; i++) {
-        board[i] = '_';
-    }
-}
-
-const char* get_random_word(const char* words[], int count) {
-    count = sizeof(*words) / sizeof(*words[0]);
-    int random_word = rand() % count;
-    const char* word = words[random_word];
-    return word;
-}
-
-void print_board(char board[], int length) {
-    for (int i = 0; i < length; i++) {
-        printf("%c", board[i]);
-        printf(" ");
-    }
-    printf("\n");
-}
 
 int main() {
     setlocale(LC_ALL, "");
@@ -46,11 +25,13 @@ int main() {
     char board[random_word_len + 1];
     for (int i = 0; i < random_word_len; i++) {
         board[i] = '_';
+
     }
 
     board[random_word_len] = '\0';
-    for (int i = 0; i < )
-
+    for (int i = 0; i < random_word_len; i++) {
+        
+    }
 
     printf("%s\n%s\n", words[random_index], board);
     
