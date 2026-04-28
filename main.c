be205f57ec5f567fd235a73df4e499c2af250aed
Fredmaster08 <frxdmaster@gmail.com>
2026-04-28 19:55:16 +0200

    HANGMAN

--- main.c
+++ main.c
@@ -6,43 +6,65 @@
 #include <locale.h>
 #include <time.h>
 
+const char* words[] = {
+    /*"hangman",
+    "Eichhörnchen",
+    "Gitarre",
+    "Flummibär",
+    "Kakerlake"
+    */
+    "did",
+    "ttchef"
+};
+
+
+
+bool has_won(const char* word, bool found_chars[26]) {
+    const int word_len = strnlen(word, 64);
+    int found_count = 0;
+    for (int i = 0; i < word_len; i++) {
+        const char c = toupper(word[i]);
+        if (found_chars[c - 'A']) found_count += 1;
+    }
+    return word_len == found_count;
+}
+
+void display(const char* word, bool found_chars[26]) {
+    const int word_len = strnlen(word, 64);
+    for (int i = 0; i < word_len; i++) {
+        const char c = toupper(word[i]);
+        printf("%c ", found_chars[c - 'A'] ? c : '_');
+    }
+    printf("\n");
+}
+
 int main() {
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
-    const char* words[] = {
-        "hangman",
-        "Eichhörnchen",
-        "Gitarre",
-        "Flummibär",
-        "Kakerlake"
-    };
-
+    bool found_chars[26] = {0};
+    
     int count = sizeof(words) / sizeof(words[0]);
     int random_index = rand() % count;
     int random_word_len = strlen(words[random_index]);
+    const char* word = words[random_index];
 
-    char board[random_word_len + 1];
-    for (int i = 0; i < random_word_len; i++) {
-        board[i] = '_';
+    display(word, found_chars);
 
-    }
+    while(1) {
 
-    board[random_word_len] = '\0';
-    printf("%s\n", words[random_index]);
-    for (int i = 0; i < random_word_len; i++) {
-        printf("%c ", board[i]);
-    }
-    fflush(stdout);
-    
-    int input = 0;
+        const char guess = toupper(getchar());
+        printf("\r");
+        fflush(stdout);
 
-    while(1)  {
-        for (int i = 0; i < random_word_len; i++) {
-            
+        found_chars[guess - 'A'] = true;
+
+        display(word, found_chars);
+        if (has_won(word, found_chars)) {
+            printf("You won!\n");
+            break;
         }
     }
-
 
     return 0;
 }
